d6f095b029ab8fa33d41bbd9800588c21e35ab28
arshaq2001 <mjmarshaq@gmail.com>
2024-03-11 20:41:16 +0530

    constants.dart added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hero_app/util/constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Hero App',
       theme: ThemeData(
         // This is the theme of your application.
@@ -28,10 +30,12 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: kPrimaryOrange),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(
+        title: 'Hero App',
+      ),
     );
   }
 }
---
+++ lib/util/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryOrange = Color.fromARGB(255, 239, 112, 32);
+const Color kPrimaryDark = Color(0xFF050b13);
+const Color kSecondaryDark = Color(0xFF101926);
+
+// Color palettes for icons
+const Color kIconOrange = Color(0xFFdf8539);
+const Color kIconWhite = Color(0xFFFFFFFF);
+const Color kIconRed = Color(0xFFea453d);
+const Color kIconBlue = Color(0xFF4986f7);
+
+// Color palette for texts
+const Color kRedText = Color(0xFFea453d);
+const Color kBlueText = Color(0xFF4986f7);
